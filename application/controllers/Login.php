7ae976f3f501417b67c0ee985ae88a7e9233c457
Isaac.montiels <isaac.montiels@hotmail.com>
2018-07-09 14:28:47 -0500

    Actualizacion 2  XD
    Actualizacion de 2

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -2,6 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
+
 	public function __construct()
 	{
 		parent::__construct();
@@ -11,31 +12,48 @@ class Login extends CI_Controller {
 	public function index()
 	{
 		$data['titulo'] = "Login";
+		$get = $this->input->get();
+		if(isset($get['error_login']) && !empty($get['error_login'])){
+			$data["error_login"] = $get["error_login"];
+		}
+		
 		$this->load->view('templates/header');
-		$this->load->view('Login/index');
+		$this->load->view('Login/index',$data);
 		$this->load->view('templates/footer');
 	}
 
+	/**
+	 * [IniciorSesion][ metodo que valida el usuario que debe ser un correo y una clave y le asigna sesion o le actualiza ]
+	 * @param [array] [Post por variable global $_POST o en CI $this->input->post() ] 
+	 * @var empty($this->input->post("email") & empty($this->input->post("clave") son obligatorias 
+	 */
 	public function IniciarSesion()
 	{	
+		// si existe $_POST['email'] & $_POST['email'] y no estan vacias entra
 		if( !empty($this->input->post()) && 
 			!empty($this->input->post("email")) && 
 			!empty($this->input->post("clave"))
 		  )
 		{ 
-			$data["Datos"] = array('email' =>  $this->input->post("email"), 'clave'=>$this->input->post("clave"));
-			$usuario = $this->Login_Model->validaDatosUsuario($data['Datos']);
+			$data["datos"] = array('email' =>  $this->input->post("email"), 
+									'clave'=>$this->input->post("clave")
+							);
+							
+			$usuario = $this->Login_Model->validaDatosUsuario($data['datos']);
 			if($usuario)
 			{	
-				$url = '?token='.$usuario[0]['token']."&id=".$usuario[0]['id'];
+				echo $url = '?token='.$usuario[0]['token']."&id=".$usuario[0]['id'];
+				
 				if ( ((int)$usuario[0]['roll']) == 0 )
 				{
-					redirect('Panel_admin'.$url,'refresh');	
+					echo $usuario[0]['roll'];
+					unset($usuario[0]['roll']);
+					redirect('Panel_admin/index'.$url,'refresh');	
 				}
-				redirect('Formularios/General'.$url,'refresh');	
+				unset($usuario[0]['roll']);
+				//redirect('Formularios/General'.$url,'refresh');	
 			}else{
-				redirect('Login','refresh');
-					
+				redirect('Login/index?error_login=acceso','refresh');
 			}
 
 		}else{
--- application/views/Login/index.php
+++ application/views/Login/index.php
@@ -2,10 +2,57 @@
 <div class="container" align="center">
 	<div class="row">
 		<div class="container">
+		<?php 
+		
+				if(isset($error_login) && $error_login=='acceso'){
+					echo '
+					<div class="col m-2 p-2 ">
+						<div class="alert alert-danger alert-dismissible fade show" role="alert">
+							<strong>Usuario o contraseña incorrecta!</strong> <br />Porfavor Trate de Nuevo
+							<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+
+					</div>
+					';
+				}
+				if(isset($error_login) && $error_login=='tiempo'){
+					echo '
+					<div class="col m-2 p-2 ">
+						<div class="alert alert-warning alert-dismissible fade show" role="alert">
+							<strong>Session Cerrada Por Inactividad</strong> <br />
+							La sesion finalizo por que hubo un tiempo largo de inactividad.<br />
+							Porfavor inicie sesion nueva mente
+							<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+
+					</div>
+					';
+				}
+				if(isset($error_login) && $error_login=='session'){
+					echo '
+					<div class="col m-2 p-2 ">
+						<div class="alert alert-warning alert-dismissible fade show" role="alert">
+							<strong>Error en la sesion</strong> <br />
+							Porfavor inicie session nuevamente.<br />
+							nota: evite modificar o manipular la URL ,eso le causara problemas
+							<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+
+					</div>
+					';
+				}
+			?>
 			<div class="col m-2 p-2 card text-white bg-dark mb-3 ">
 				<form action="<?php echo base_url()."Login/IniciarSesion" ?>" method="post" class="card-body" >
   					<div class="card-header">Iniciar Sesión</div>
 					<div class="input-group p-1 input-group-md mb-2">
+					
 						<div class="input-group-prepend">
 							<span class="input-group-text" id="Email">Email</span>
 						</div>
@@ -16,7 +63,7 @@
 						<div class="input-group-prepend">
 							<span class="input-group-text" id="password">Contraseña</span>
 						</div>
-						<input type="password" class="form-control" name="password" placeholder="password" aria-label="password" aria-describedby="Email" required>
+						<input type="password" class="form-control" name="clave" placeholder="password" aria-label="password" aria-describedby="Email" required>
 					</div>
 		
 	
