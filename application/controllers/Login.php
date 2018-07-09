e922706493e3aff2196a551d6a58497227c79adc
KappaShmuel <40840130+KappaShmuel@users.noreply.github.com>
2018-07-08 21:55:59 -0500

    Ejemplo
    Casi listo

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -18,36 +18,36 @@ class Login extends CI_Controller {
 
 	public function index()
 	{
-		$data['titulo'] = "Login";
+		$data['titulo'] = "Registro";
 		$this->load->view('templates/header');
-		$this->load->view('Login/vlogin');
+		$this->load->view('Registro/vlogin');
 		$this->load->view('templates/footer');
 	}
 
 	public function ingresoLogin()
-	{	
-		if( !empty($this->input->post()) && 
-			!empty($this->input->post("email")) && 
+	{
+		if( !empty($this->input->post()) &&
+			!empty($this->input->post("email")) &&
 			!empty($this->input->post("clave"))
 		  )
 		{
 			$data["Datos"] = array('email' =>  $this->input->post("email"), 'clave'=>$this->input->post("clave"));
 			$usuario = $this->Login_Model->validaDatosUsuario($data['Datos']);
 			if($usuario)
-			{	
+			{
 				$url = '?token='.$usuario[0]['token']."&id=".$usuario[0]['id'];
 				if ( ((int)$usuario[0]['roll']) == 0 )
 				{
-					redirect('Panel_admin'.$url,'refresh');	
+					redirect('Panel_admin'.$url,'refresh');
 				}
-				redirect('Formularios/General'.$url,'refresh');	
+				redirect('Formularios/General'.$url,'refresh');
 			}else{
-				redirect('Login','refresh');
-					
+				redirect('Registro','refresh');
+
 			}
 
 		}else{
-			redirect('Login','refresh');
+			redirect('Registro','refresh');
 		}
 	}
 }
