f76e942731fde9b2a7fa3f69a37fe63e00e88480
KappaShmuel <40840130+KappaShmuel@users.noreply.github.com>
2018-07-11 03:17:16 -0500

    Revert "Estenosirve"
    This reverts commit 8137c0871babe7f41c18ef730b3fb49f9548ac0b.

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -30,7 +30,10 @@ class Login extends CI_Controller {
 			$data["error_login"] = $get["error_login"];
 		}
 		
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 8137c08... Estenosirve
 		$this->load->view('templates/header');
 		$this->load->view('Login/index',$data);
 		$this->load->view('templates/footer');
@@ -38,24 +41,30 @@ class Login extends CI_Controller {
 
 	/**
 	 * [IniciorSesion][ metodo que valida el usuario que debe ser un correo y una clave y le asigna sesion o le actualiza ]
-	 * @param [array] [Post por variable global $_POST o en CI $this->input->post() ]
-	 * @var empty($this->input->post("email") & empty($this->input->post("clave") son obligatorias
+	 * @param [array] [Post por variable global $_POST o en CI $this->input->post() ] 
+	 * @var empty($this->input->post("email") & empty($this->input->post("clave") son obligatorias 
 	 */
 	public function IniciarSesion()
-	{
+	{	
 		// si existe $_POST['email'] & $_POST['email'] y no estan vacias entra
+<<<<<<< HEAD
 		if( !empty($this->input->post()) &&
 			!empty($this->input->post("email")) &&
 
+=======
+		if( !empty($this->input->post()) && 
+			!empty($this->input->post("email")) && 
+>>>>>>> parent of 8137c08... Estenosirve
 			!empty($this->input->post("clave"))
 		  )
-		{
-			$data["datos"] = array('email' =>  $this->input->post("email"),
+		{ 
+			$data["datos"] = array('email' =>  $this->input->post("email"), 
 									'clave'=>$this->input->post("clave")
 							);
-
+							
 			$usuario = $this->Login_Model->validaDatosUsuario($data['datos']);
 			if($usuario)
+<<<<<<< HEAD
 
 			{
 				$url = '?token='.$usuario[0]['token']."&id=".$usuario[0]['id'];
@@ -69,16 +78,19 @@ class Login extends CI_Controller {
 
 
 			{
+=======
+			{	
+>>>>>>> parent of 8137c08... Estenosirve
 				echo $url = '?token='.$usuario[0]['token']."&id=".$usuario[0]['id'];
-
+				
 				if ( ((int)$usuario[0]['roll']) == 0 )
 				{
 					echo $usuario[0]['roll'];
 					unset($usuario[0]['roll']);
-					redirect('Panel_admin/index'.$url,'refresh');
+					redirect('Panel_admin/index'.$url,'refresh');	
 				}
 				unset($usuario[0]['roll']);
-				//redirect('Formularios/General'.$url,'refresh');
+				//redirect('Formularios/General'.$url,'refresh');	
 			}else{
 				redirect('Login/index?error_login=acceso','refresh');
 
