0b79a093da54cfd3644c1ac95a042e14025e3b0d
D1s4st33r <masterchif57@gmail.com>
2018-07-15 16:31:11 -0500

    :v

--- application/views/PanelControl/components/cateSeccyPre.php
+++ application/views/PanelControl/components/cateSeccyPre.php
@@ -81,12 +81,49 @@
 <?php endif; ?>
 
 <?php if($config=="uppregunta"): ?>
-<div class=" col-md">
-       <div class="form-group" style="float:left;">
-       <label for="categoria"><b> nueva categoria</b></label>
-       <input type="text" name="categoria" class="form-control-sm" id="categoria" >
-       </div>
-       <button type="button" class="btn btn-danger btn-sm" style="float:right;">cancelar</button>
-       <button type="button" class="btn btn-primary btn-sm" style="float:right;">aceptar</button>
+
+<div class="container">
+    <div class="row m-1 border rounded  pt-2 pb-2">
+        <div class=" col-sm col-md-6 col-lg-3 ">
+            <div class="form-group">
+            <label for="nombre"><b> Nombre</b></label>
+            <input type="text" class="form-control" id="nombre" name="nombre">
+            </div>
+        </div>
+
+        <div class=" col-sm col-md-6 col-lg-3 ">
+            <div class="form-group">
+            <label for="apellido"><b> Apellido</b></label>
+            <input type="text" name="apellido" class="form-control" id="apellido" >
+            </div>
+        </div>
+
+        <div class=" col-sm col-md-6 col-lg-3 ">
+            <div class="form-group">
+            <label for="email"><b> Email</b></label>
+            <input type="mail" name="email" class="form-control" id="email"  >
+            </div>
+        </div>
+        
+        <div class=" col-sm col-md-6 col-lg-3 ">
+            <div class="form-group">
+            <label for="telefono"><b> Telefono</b></label>
+            <input type="text" class="form-control" name="telefono" id="telefono" >
+            </div>
+        </div>
+        <div class=" col-sm col-md-6 col-lg-3 ">
+            <div class="form-group">
+            <label for="contrasena"><b> Contraseña </b></label>
+            <input type="password" class="form-control" name="clave" id="contrasena" >
+            </div>
+        </div>
+      
+
+      <div class=" col-12 ">
+        <div class="form-group">
+        <button  type="button" class="btn btn-primary" onclick="return AgregarUsuario('<?php echo base_url("AgregarContador").$session;?>')" >  <i class="fas fa-thumbs-up fa-md"></i> Registrar Contador</button> 
+        </div>
+      </div>  
+    </div>
 </div>
 <?php endif; ?>
