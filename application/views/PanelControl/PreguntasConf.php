f4dd314a81e7594ced1c315eb06afdf7eda820bd
D1s4st33r <masterchif57@gmail.com>
2018-07-16 13:51:29 -0500

    dale

--- application/views/PanelControl/PreguntasConf.php
+++ application/views/PanelControl/PreguntasConf.php
@@ -1,33 +1,3 @@
-<?php
-  if(!isset($usuario)){ redirect('Login/index?error_login=session','refresh');  }
-?>
- <?php
-      $this->load->view("PanelControl/components/PanelMenu",array("usuario"=>$usuario,"session"=>$session));
-  ?>
-  <style>
-          .grupo-bot{
-            
-            text-align: right;
-            float:left;
-            margin-right:0;
-          }
-        </style>
-<div role="main" class="container">
-<div class="d-flex align-items-center p-3 my-3 text-white-50 bg-lgBlue rounded box-shadow">
-        <!-- <img class="mr-3" src="https://getbootstrap.com/docs/4.1/assets/brand/bootstrap-outline.svg" alt="" width="48" height="48"> -->
-    <div class="lh-100">
-      <h6 class="mb-0 text-white lh-100">ISAAC MONTIEL</h6>
-          <small>Administrador</small>
-      </div>
- </div>
-
-<div class="my-3 p-3 bg-white rounded box-shadow container" id="perfil">
-  <h6 class="border-bottom border-gray pb-2 mb-0">GENERAL ESPECIFICATION</h6>
-  <div class="row">
-    
-</div>
-</div>
-
 <div class="container p-1" style="margin-bottom:60px;background:white;">
   		<div class="container">
 	  		<nav class="navbar navbar-expand-lg navbar-light ">
