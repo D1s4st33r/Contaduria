152e320775a7836eef6a634217130057067c8f61
D1s4st33r <masterchif57@gmail.com>
2018-07-25 15:29:24 -0500

    ds

--- application/views/PanelControl/components/solicitudArchivo.php
+++ application/views/PanelControl/components/solicitudArchivo.php
@@ -1,4 +1,4 @@
-<?php if($config=="addpregunta"): ?>
+<?php if($soli=="1"): ?>
 <div class="custom-file">
     <input class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01" type="file">
     <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
